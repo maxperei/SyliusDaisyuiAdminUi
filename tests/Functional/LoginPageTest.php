fd15f6f7d84044c3a94739967e20a21fdd596473
maxperei <hello@maxperei.info>
2026-02-10 12:46:04 +0100

    misc(e2e): [login] take screenshots w/ panther

--- tests/Functional/LoginPageTest.php
+++ tests/Functional/LoginPageTest.php
@@ -4,23 +4,35 @@ declare(strict_types=1);
 
 namespace Tests\Sylius\DaisyuiAdminUi\Functional;
 
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Panther\Client as PantherClient;
+use Symfony\Component\Panther\PantherTestCase;
 
-class LoginPageTest extends WebTestCase
+class LoginPageTest extends PantherTestCase
 {
-    private KernelBrowser $client;
+    private PantherClient $client;
 
     protected function setUp(): void
     {
-        $this->client = self::createClient();
+        $this->client = static::createPantherClient();
     }
 
     public function testLoginTemplate(): void
     {
         $this->client->request('GET', '/admin/login');
 
+        $this->assertSelectorAttributeContains('body', 'class', 'flex flex-column');
         $this->assertSelectorTextContains('h2.card-title', 'sylius.ui.login_to_your_account');
         $this->assertSelectorTextContains('button[type=submit]', 'sylius.ui.login');
+
+        $this->switchThemeAndTakeScreenshots();
+    }
+
+    private function switchThemeAndTakeScreenshots(): void
+    {
+        $this->client->request('GET', '/admin/login');
+
+        $this->client->takeScreenshot('screens/login-page--light.png');
+        $this->client->getCrawler()->filter('label.toggle')->click();
+        $this->client->takeScreenshot('screens/login-page--dark.png');
     }
 }
