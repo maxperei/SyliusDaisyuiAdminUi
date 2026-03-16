8c63a0898781de47074bc6f521eb11f7393d0810
maxperei <hello@maxperei.info>
2026-03-16 17:32:46 +0100

    feat(layout): [dashboard] wip drawer hooks

--- config/app/twig_hooks/layout.php
+++ config/app/twig_hooks/layout.php
@@ -12,21 +12,9 @@ return static function (ContainerConfigurator $configurator): void {
                     'template' => '@SyliusDaisyuiAdminUi/shared/layout/title.html.twig',
                 ],
             ],
-            /* @todo: unified webpack encore config (or keep tailwindcli?) */
-            'sylius_admin.base#stylesheets' => [
-                'stylesheets' => [
-                    'enabled' => false,
-                ],
-            ],
             'sylius_admin.base#importmap' => [
                 'importmap' => [
                     'template' => '@SyliusDaisyuiAdminUi/shared/layout/importmap.html.twig',
-                ],
-            ],
-            /* @todo: unified webpack encore config (or create a terser one?) */
-            'sylius_admin.base#javascripts' => [
-                'javascripts' => [
-                    'enabled' => false,
                 ],
             ],
         ],
